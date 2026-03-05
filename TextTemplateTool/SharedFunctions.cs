ace973bfa3f8b28b4a87836756795082401d5e81
GeorgeBennett <GBennettOfficial@outlook.com>
2026-03-04 19:54:45 -0500

    sql name function and property model

---
+++ TextTemplateTool/SharedFunctions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextTemplateTool
+{
+    internal class SharedFunctions
+    {
+        public static string ToSqlType(Type type, int? varcharSize)
+        {
+            bool isNullable = Nullable.GetUnderlyingType(type) != null;
+            string strIsNullable = isNullable ? " NULL" : "";
+            type = Nullable.GetUnderlyingType(type) ?? type;
+
+            if (type == typeof(string))
+                return $"VARCHAR({varcharSize?.ToString() ?? "255"})" + strIsNullable;
+
+            else if (type == typeof(int))
+                return "INT" + strIsNullable;
+
+            else if (type == typeof(long))
+                return "BIGINT" + strIsNullable;
+
+            else if (type == typeof(bool))
+                return "BIT" + strIsNullable;
+
+            else if (type == typeof(decimal))
+                return "DECIMAL(18,2)" + strIsNullable;
+
+            else if (type == typeof(double))
+                return "FLOAT" + strIsNullable;
+
+            else if (type == typeof(float))
+                return "REAL" + strIsNullable;
+
+            else if (type == typeof(DateTime))
+                return "DATETIME" + strIsNullable;
+
+            else if (type == typeof(Guid))
+                return "UNIQUEIDENTIFIER" + strIsNullable;
+
+            else if (type == typeof(byte[]))
+                return "VARBINARY(MAX)" + strIsNullable;
+
+            else if (type == typeof(short))
+                return "SMALLINT" + strIsNullable;
+
+            else if (type == typeof(byte))
+                return "TINYINT" + strIsNullable;
+
+            else if (type == typeof(TimeSpan))
+                return "TIME" + strIsNullable;
+
+            return "VARCHAR(255)";
+        }
+    }
+}
---
+++ TextTemplateTool/Sql/Models/PropertyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextTemplateTool.Sql.Models
+{
+    internal record PropertyModel(string Name, Type Type, IReadOnlyList<string> AttributeData)
+    {
+        public string SqlType => SharedFunctions.ToSqlType(Type);
+        public string SqlNull => Nullable.GetUnderlyingType(Type) != null ? "NULL" : "NOT NULL";
+    }
+}
